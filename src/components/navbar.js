a07854b55526762643a80eb865b188c294ade500
Roy-maker <carsanchezsie@gmail.com>
2024-09-27 11:19:53 -0600

    Navbar responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,8 +6,8 @@ import logo from '../images/logo.jpeg';
 
 const Navbar = ({ toggleTheme, currentTheme }) => {
   const { t, i18n } = useTranslation("global");
-
   const [language, setLanguage] = useState(i18n.language); 
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleLanguage = () => {
     const newLanguage = language === 'en' ? 'es' : 'en';
@@ -15,28 +15,38 @@ const Navbar = ({ toggleTheme, currentTheme }) => {
     setLanguage(newLanguage);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Abre o cierra el menú
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src={logo} alt="Logo" width="50" />
       </div>
-      
-      <ul className="navbar-menu">
-        <li><Link to="/home">{t("navbar.home")}</Link></li>
-        <li><Link to="/services">{t("navbar.services")}</Link></li>
-        <li><Link to="/projects">{t("navbar.projects")}</Link></li>
-        <li><Link to="/certifications">{t("navbar.certifications")}</Link></li>
-        <li><Link to="/clients">{t("navbar.clients")}</Link></li>
-        <li><Link to="/contact">{t("navbar.contact")}</Link></li>
-      </ul>
 
-      <div className="navbar-actions">
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <span className={menuOpen ? 'bar open' : 'bar'}></span>
+        <span className={menuOpen ? 'bar open' : 'bar'}></span>
+        <span className={menuOpen ? 'bar open' : 'bar'}></span>
+      </div>
+
+      <ul className={`navbar-menu ${menuOpen ? 'open' : ''}`}>
+        <li><Link to="/home">{t('navbar.home')}</Link></li>
+        <li><Link to="/services">{t('navbar.services')}</Link></li>
+        <li><Link to="/projects">{t('navbar.projects')}</Link></li>
+        <li><Link to="/certifications">{t('navbar.certifications')}</Link></li>
+        <li><Link to="/clients">{t('navbar.clients')}</Link></li>
+        <li><Link to="/contact">{t('navbar.contact')}</Link></li>
+      </ul>
+      
+      <div className={`navbar-actions ${menuOpen ? 'open' : ''}`}>
         <button onClick={toggleTheme}>
-          {currentTheme === 'light' ? t("navbar.dark-mode") : t("navbar.light-mode")}
+          {currentTheme === 'light' ? t('navbar.dark-mode') : t('navbar.light-mode')}
         </button>
 
         <button onClick={toggleLanguage}>
-        {language === 'en' ? t("navbar.spanish") : t("navbar.english")}
+          {language === 'en' ? t('navbar.spanish') : t('navbar.english')}
         </button>
       </div>
     </nav>
