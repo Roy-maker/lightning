d8ec852c5c0952f61c59f4cb66cb619a9b8d8672
SnipperPlayer <galeana2002@hotmail.com>
2024-10-11 10:15:56 -0700

    Add Head and move components

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from "react-i18next";
 import CardsHome from '../components/cardsHome';
 import CarouselProt from '../components/CarouselProt';
+import Head from '../components/Head';      
 
 
 const Home = () => {
@@ -10,6 +11,8 @@ const Home = () => {
   return (
     <div>
       <h1>{t("home.welcome")}</h1> 
+      <Head/>
+      <h1>Especialista En Sistemas De Pararrayos</h1>
       <CardsHome/>
       <h1>Protección Efectiva</h1>
       <CarouselProt/>
