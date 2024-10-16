206a0a09c30b8423535325cbb0cac398a8701fac
Roy-maker <carsanchezsie@gmail.com>
2024-10-16 11:49:10 -0600

    Contact info design

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useTranslation } from "react-i18next";
 import './css/contactInfo.css';
+import logo from '../images/test.png';
 
 const ContactInfo = () => {
   const { t } = useTranslation("global");
 
   return (
+    <div className='contact-info-container'>
+    
     <div className="contact-info">
       <h2>{t('contact.title')}</h2>
       <p><strong>{t('contact.email')}: </strong>your-email@example.com</p>
@@ -22,6 +25,10 @@ const ContactInfo = () => {
           <i className="fab fa-linkedin"></i> LinkedIn
         </a>
       </div>
+    </div>
+    <div>
+    <img src={logo} alt="Logo" width="300" />  
+    </div>
     </div>
   );
 };
